72cdc549f6d51f915e5af223a8ae7420bedc28a6
George Tokatlidis <155269445+george-toka@users.noreply.github.com>
2025-09-17 02:36:30 +0300

    Update polytrope_sim.jl

--- julia_version/fastv1_kd&single_oc/polytrope_sim.jl
+++ julia_version/fastv1_kd&single_oc/polytrope_sim.jl
@@ -236,7 +236,7 @@ module polytropeSim
 			if keepSnaps * intervalCounter == snapInterval || t >= tEnd
 				
 				# Use r_com to sample radial density profile starting from the true center
-				rr[:, 1] = rlin .- rcom_x 	# samples lie on the x-axis
+				rr[:, 1] = rlin .+ rcom_x 	# samples lie on the x-axis
 				rr[:, 2:3] .= r_com[:, 2:3]
 				rho_radial = HJL.density_plot(m, rr, pos, Kh)
 				R = find_star_radius(rlin, rho_radial; threshold=0.01*rho_radial[1])
